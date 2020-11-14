ac08ee49490f41a4df7e28bf31b54b6fa8051805
Han Jang Hyun <wkdgus116490@gmail.com>
2020-11-15 00:53:28 +0900

    resize off element

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-(function(window, document) {
+(function (window, document) {
     'use strict';
 
     const $toggles = document.querySelectorAll('.toggle');
@@ -8,9 +8,21 @@
         toggleElements();
     });
 
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 1024) {
+            offElements();
+        }
+    })
+
     const toggleElements = () => {
         [].forEach.call($toggles, toggle => {
             toggle.classList.toggle('on');
+        });
+    }
+
+    const offElements = () => {
+        [].forEach.call($toggles, toggle => {
+            toggle.classList.remove('on');
         });
     }
 })(window, document)
